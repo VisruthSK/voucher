e61a9f49dea277519e3fc6a6e76e96b5e9705b31
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2026-02-23 12:29:11 -0800

    Super basic use_vouch

--- R/vouch.R
+++ R/vouch.R
@@ -1,10 +1,33 @@
-use_vouch <- function(path = ".") {
-  if (
-    file.exists("VOUCHED.td") ||
-      file.exists(".github/VOUCHED.td")
-  ) {
+use_vouch <- function() {
+  if (fs::file_exists(c("VOUCHED.td", ".github/VOUCHED.td")) |> any()) {
     message("Existing vouch database found. Exiting without any changes.")
     return()
   }
-  file.create()
+
+  path <- fs::path(".github", "VOUCHED.td")
+  fs::dir_create(fs::path_dir(path), recurse = TRUE)
+
+  default_vouched_text <- c(
+    "# Copied from vouch repo:",
+    "# Vouched contributors for this project.",
+    "#",
+    "# See https://github.com/mitchellh/vouch for details.",
+    "#",
+    "# Syntax:",
+    "#   - One handle per line (without @), sorted alphabetically.",
+    "#   - Optional platform prefix: platform:username (e.g., github:user).",
+    "#   - Denounce with minus prefix: -username or -platform:username.",
+    "#   - Optional details after a space following the handle.",
+    ""
+  )
+  writeLines(default_vouched_text, path)
+
+  if (
+    !fs::file_exists(".Rbuildignore") ||
+      !("^\\.github$" %in% readLines(".Rbuildignore", warn = FALSE))
+  ) {
+    cat("^\\.github$", "\n", file = ".Rbuildignore", append = TRUE, sep = "")
+  }
+
+  invisible(NULL)
 }
